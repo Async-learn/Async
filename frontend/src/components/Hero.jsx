268b89f248e253d3669d51b9a4941a39a9566c71
George Olufemi <georgeoluolufemi@gmail.com>
2022-08-17 11:27:11 +0100

    Added Favicon, removed redundant code, added finishing touches to Animation and Layout

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,86 +13,109 @@ const Hero = () => {
         <div className="w-screen h-auto bg-[#DDF6FF]">
             <HomeNavbar />
             <div className="h-[454px] md:mt-[76px] md:mx-[41px] md:flex md:justify-between">
-                <div className="h-[263px] pt-[70px] md:pt-[95.5px] mb-[76px]">
+                <div className="h-[263px] pt-[70px] md:pt-\[95\.5px\] mb-[76px] md:pl-[20px]">
                     <h1 className="text-center md:text-start md:font-[600] md:text-[52px] md:leading-[78px] text-[#010914] text-[20px] leading-[30px] font-[600]">Learn <span className="font-[700]">web</span> development...</h1>
                     <p className="font-[500] text-[16px] leading-[24px] text-center pt-[24px] block md:hidden">Gain the professional knowledge you<br /> need for front-end web development<br /> with us at <span className="text-[#001933] font-[600]"><i>async</i></span></p>
                     <p className="hidden md:block font-[500] text-[20px] leading-[164%] text-[#000000] pt-[6px] md:pt-0">Gain the professional knowledge you need for front-end<br /> web development with us at <span className="text-[#001933] font-[600]"><i>async</i></span></p>
                     {/* button */}
-                    <div className="pt-[78px] md:pt-[52px]">
+                    <div className="pt-[61px] md:pt-[52px]">
                         {heroSignupButton}
                     </div>
                 </div>
                 {/*  */}
                 <div>
                     <div className="h-[454px]">
-                        <img className="block md:hidden pl-[46px]"  src="/illustrationmobile.png" alt="illustration" />
+                        <img className="block md:hidden pl-[33px]"  src="/illustrationmobile.png" alt="illustration" />
                         {/* mobile */}
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 0.9, duration: 5 }}
-                            className="block md:hidden absolute left-[250px] top-[440px]" src="/htmlmobile.png" alt="html" />
+                            className="block md:hidden absolute left-[250px] top-[440px]" 
+                            src="/htmlmobile.png" 
+                            alt="html" />
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 2.0, duration: 5 }}
-                            className="block md:hidden absolute left-[300px] top-[550px]" src="/cssmobile.png" alt="css" />
+                            className="block md:hidden absolute left-[290px] top-[550px]" 
+                            src="/cssmobile.png" 
+                            alt="css" />
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 3.0, duration: 5 }}
-                            className="block md:hidden absolute left-[240px] top-[650px]" src="/jsmobile.png" alt="js" />
+                            className="block md:hidden absolute left-[240px] top-[650px]" 
+                            src="/jsmobile.png" 
+                            alt="js" />
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 4.0, duration: 5 }}
-                            className="block md:hidden absolute left-[79px] top-[650px]" src="/reactmobile.png" alt="react" />
+                            className="block md:hidden absolute left-[68px] top-[650px]" 
+                            src="/reactmobile.png" 
+                            alt="react" />
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 5.0, duration: 5 }}
-                            className="block md:hidden absolute left-[23px] top-[550px]" src="/nodemobile.png" alt="node" />
+                            className="block md:hidden absolute left-[13px] top-[550px]" 
+                            src="/nodemobile.png" 
+                            alt="node" />
                         <motion.img
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            transition={{ delay: 6.0, duration:  5 }}
-                            className="block md:hidden absolute left-[62px] top-[440px]" src="/pythonmobile.png" alt="python" />
+                            transition={{ delay: 6.5, duration:  5 }}
+                            className="block md:hidden absolute left-[58px] top-[440px]" 
+                            src="/pythonmobile.png" 
+                            alt="python" />
                         {/* desktop */}
                         <img className="hidden md:block absolute left-[871px] top-[184px]"  src="/illustrationdesktop.png" alt="illustration" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 0.9, duration: 5 }}
-                            whileHover={{ scale: 1.2 }}
-
-                            className="hidden md:block absolute left-[1200px] top-[200px]" src="/html.png" alt="html" />
+                            className="hidden md:block absolute left-[1200px] top-[200px]" 
+                            src="/html.png" 
+                            alt="html" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 2.0, duration: 5 }}
-                            className="hidden md:block absolute left-[1257px] top-[370px]" src="/css.png" alt="css" />
+                            className="hidden md:block absolute left-[1257px] top-[370px]" 
+                            src="/css.png" 
+                            alt="css" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 3.0, duration: 5 }}
-                            className="hidden md:block absolute left-[1190px] top-[539px]" src="/js.png" alt="javascript" />
+                            className="hidden md:block absolute left-[1190px] top-[539px]" 
+                            src="/js.png" 
+                            alt="javascript" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 4.0, duration: 5 }}
-                            className="hidden md:block absolute left-[870px] top-[539px]" src="/react.png" alt="react" />
+                            className="hidden md:block absolute left-[870px] top-[539px]" 
+                            src="/react.png" 
+                            alt="react" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             transition={{ delay: 5.0, duration: 5 }}
-                            className="hidden md:block absolute left-[810px] top-[370px]" src="/node.png" alt="node" />
+                            className="hidden md:block absolute left-[810px] top-[370px]" 
+                            src="/node.png" 
+                            alt="node" />
                         <motion.img 
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            transition={{ delay: 6.0, duration: 5 }}
-                            className="hidden md:block absolute left-[850px] top-[200px]" src="/python.png" alt="python" />
+                            transition={{ delay: 6.5, duration: 5 }}
+                            className="hidden md:block absolute left-[850px] top-[200px]" 
+                            src="/python.png" 
+                            alt="python" />
                     </div>
                 </div>
+                
             </div>
         </div>
     </React.Fragment>
