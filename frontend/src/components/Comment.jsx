ae7483046d6d868e8f2775062d5edc90c652398d
j0n-3kw3r3 <johnebong@gmail.com>
2022-08-19 15:17:33 +0100

    corrections to comment section

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -5,25 +5,25 @@ import CommentCard from './CommentCard';
 const Comment = () => {
   const [comments, setComments] = useState([
     {
-      comment: '“Async provides the ability to learn web development with ease and step by step guidance frombeginner to  milestone level.’’',
+      comment: '“Async provides the ability to learn web development with ease and step by step guidance frombeginner to  milestone level."',
       name: 'John E, Dev Ops',
       occupation:'Freelance Developer',
       url:'/Frame 33.png'
     },
     {
-      comment: '“Async provides the ability to learn web development with ease and step by step guidance frombeginner to  milestone level.’’',
+      comment: '"Async provides the ability to learn web development with ease and step by step guidance frombeginner to  milestone level."',
       name: 'John E, Dev Ops',
       occupation: 'Freelance Developer',
       url:'/Frame 331.png'
     },
     {
-      comment: '““Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.”’’',
+      comment: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.”',
       name: 'Etieno M, Frontend Dev',
       occupation:'ASYNC',
       url:'/Frame 334.png'
     },
     {
-      comment: '““Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.”’’',
+      comment: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."',
       name: 'Etieno M, Frontend Dev',
       occupation:'ASYNC',
       url:'/Frame 332.png'
@@ -32,7 +32,7 @@ const Comment = () => {
   return (
     
     <>
-      <div className=' grid md:grid-cols-2  p-[68px] comment md:w-full  md:h-[534px]  '>
+      <div className=' grid md:grid-cols-2  py-[1.25rem] md:px-[2.5rem] md:py-[4.25rem] px-[1.5rem] text-[#001933;] comment md:w-full w-full  md:h-[534px] xl:gap-x-[3rem] md:gap-x-[2rem] '>
 
         {
           comments.map((comment, key) => {
--- frontend/src/components/CommentCard.jsx
+++ frontend/src/components/CommentCard.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 function CommentCard({ comment, name, occupation, url }) {
   return (
     <div>
-      <div className='flex  h-[160px]  mb-8 mr-[53px]  md:mb-[78px] 2xl:h-[160px] xl:h-[180px]'>
-        <div className=' md:w-[160px]   w-[60px] h-[60px]   shrink-0 mr-4 md:mr-[48px] mt-2   '>
+      <div className='flex mb-[2.38rem]   md:mb-[4.88rem]   xl:w-full xl:gap-[3rem]   md:h-[10rem] h-[9.69rem] w-full'>
+
+        <div className=' md:w-[10rem] md:h-[10rem]  w-[60px] h-[60px] md:mr-[3rem]   shrink-0 mr-4  mt-2   '>
           <img src={url} alt="img" className=' rounded-full border-2 border-[#001933] object-contain' />
         </div>
-        <div className=' grid gap-5 md:gap-20   grid-rows-2 p-2'>
-          <div>
-            <p className=' md:text-[14px]  border-b border-[#001933]  pb-6 md:pb-2 lg:pb-4 md:mb-10 pt-2'>{comment}</p>
+
+        <div className=' md:grid md:gap-[5rem]  grid-rows-2 '>
+          <div className='md:text-[14px]  border-b border-[#001933] py-[.63rem] mb-[.5rem]  lg:pb-4 md:mb-10 h-[6.63rem] w-full flex pb-[.63rem]  md:pb-[1.83rem]   2xl:pb-[1.63rem]  overflow-hidden '>
+            <p className=' text-[0.7rem] md:text-[1.2rem] 2xl:text-[1.4rem] 2xl:leading-[2.8rem] leading-[1.5rem] '>{comment}</p>
           </div>
 
           <div>
-            <h1 className=' md:text-xl text-[14px] font-semibold mb-1'>{name}</h1>
-            <h2 className=' text-[10px]'>{occupation}</h2>
+            <h1 className=' md:text-[1.25rem] 2xl:text-[1.8rem] text-[0.88rem] font-[500] '>{name}</h1>
+            <h2 className=' text-[10px] md:text-[.88rem] font-semibold 2xl:text-[1.2rem] text-[#576A7B]'>{occupation}</h2>
           </div>
 
         </div>
