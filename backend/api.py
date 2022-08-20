de1b2e887f53e3577ae0c960cb7781062eb23cde
kelani34 <kelanitaiwo34@gmail.com>
2022-08-20 17:08:23 +0100

    updated the create_app function

--- backend/api.py
+++ backend/api.py
@@ -25,129 +25,131 @@ def user_paginated(request, selection):
 
     return current_user
 
-# create and configure the app
-app = Flask(__name__)
-setup_db(app)
+def create_app():
 
-# set up CORS, allowing all origins
-CORS(app, resources={'/': {'origins': '*'}})
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
 
-
-@app.after_request
-def after_request(response):
-    '''
-    Sets access control.
-    '''
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods',
-                         'GET,POST,DELETE,PUT,OPTIONS')
-    return response
+    # set up CORS, allowing all origins
+    CORS(app, resources={'/': {'origins': '*'}})
 
 
-
-@app.route('/users')
-def get_users():
-    ''' Renders all users paginated to 10 user per page'''
-    selection = User.query.order_by(User.id).all()
-    total_users = len(selection)
-    current_users = user_paginated(request, selection)
-
-    if len(current_users) == 0:
-        abort(404)
-
-    return jsonify({
-        'success': True,
-        'users': current_users,
-        'total_users': total_users
-    })
+    @app.after_request
+    def after_request(response):
+        '''
+        Sets access control.
+        '''
+        response.headers.add('Access-Control-Allow-Headers',
+                            'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                            'GET,POST,DELETE,PUT,OPTIONS')
+        return response
 
 
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    ''' Renders Delete for a particular user'''
-    try:
-        user = User.query.filter(
-            User.id == user_id).one_or_none()
 
-        if user is None:
+    @app.route('/users')
+    def get_users():
+        ''' Renders all users paginated to 10 user per page'''
+        selection = User.query.order_by(User.id).all()
+        total_users = len(selection)
+        current_users = user_paginated(request, selection)
+
+        if len(current_users) == 0:
             abort(404)
 
-        user.delete()
-        selection = User.query.order_by(User.id).all()
-        current_user = user_paginated(request, selection)
-
-        return jsonify(
-            {
-                'success': True,
-                'deleted': user_id,
-                'user': current_user,
-                'total_user': len(User.query.all())}
-        )
-    except:
-        abort(422)
+        return jsonify({
+            'success': True,
+            'users': current_users,
+            'total_users': total_users
+        })
 
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    ''' Renders avalability to create new user'''
-    body = request.get_json()
+    @app.route('/users/<int:user_id>', methods=['DELETE'])
+    def delete_user(user_id):
+        ''' Renders Delete for a particular user'''
+        try:
+            user = User.query.filter(
+                User.id == user_id).one_or_none()
 
-    first_name = body.get('first_name')
-    last_name = body.get('last_name')
-    email = body.get('email')
-    try:
-        user = User(
-            first_name=first_name,
-            last_name=last_name,
-            email=email)
-        user.insert()
+            if user is None:
+                abort(404)
 
-        selection = User.query.order_by(User.id).all()
-        if last_name is None or first_name is None or email is None:
-            abort(404)
-        current_user = user_paginated(request, selection)
+            user.delete()
+            selection = User.query.order_by(User.id).all()
+            current_user = user_paginated(request, selection)
 
-        return jsonify(
-            {
-                'success': True,
-                'created': user.id,
-                'user': current_user,
-                'total_users': len(User.query.all()),
-            }
-        )
-    except:
-        abort(422)
-
-# Error handllerss for each error that may occur
+            return jsonify(
+                {
+                    'success': True,
+                    'deleted': user_id,
+                    'user': current_user,
+                    'total_user': len(User.query.all())}
+            )
+        except:
+            abort(422)
 
 
-@app.errorhandler(404)
-def handle_not_found_error404(e):
-    return jsonify({
-        "success": False,
-        "error": 404,
-        "message": "resource not found"
-    }), 404
+    @app.route('/users', methods=['POST'])
+    def create_user():
+        ''' Renders avalability to create new user'''
+        body = request.get_json()
+
+        first_name = body.get('first_name')
+        last_name = body.get('last_name')
+        email = body.get('email')
+        try:
+            user = User(
+                first_name=first_name,
+                last_name=last_name,
+                email=email)
+            user.insert()
+
+            selection = User.query.order_by(User.id).all()
+            if last_name is None or first_name is None or email is None:
+                abort(404)
+            current_user = user_paginated(request, selection)
+
+            return jsonify(
+                {
+                    'success': True,
+                    'created': user.id,
+                    'user': current_user,
+                    'total_users': len(User.query.all()),
+                }
+            )
+        except:
+            abort(422)
+
+    # Error handllerss for each error that may occur
 
 
-@app.errorhandler(400)
-def handle_bad_request_error400(e):
-    return jsonify({
-        "success": False,
-        "error": 400,
-        "message": "bad request"
-    }), 400
+    @app.errorhandler(404)
+    def handle_not_found_error404(e):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
 
 
-@app.errorhandler(422)
-def handle_unprocessable_error422(e):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
+    @app.errorhandler(400)
+    def handle_bad_request_error400(e):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    @app.errorhandler(422)
+    def handle_unprocessable_error422(e):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    return app
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)
